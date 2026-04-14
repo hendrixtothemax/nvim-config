5eda424e46ca471b9c113565cbe3405da7a48f9f
Alexander Hendrix <hendrixtothemax38@gmail.com>
2026-04-14 00:04:47 -0600

    init commit with super hotkeys for windows

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,9 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- Window navigation
+vim.keymap.set("n", "<leader>h", "<C-w>h", { desc = "Go to left window" })
+vim.keymap.set("n", "<leader>j", "<C-w>j", { desc = "Go to lower window" })
+vim.keymap.set("n", "<leader>k", "<C-w>k", { desc = "Go to upper window" })
+vim.keymap.set("n", "<leader>l", "<C-w>l", { desc = "Go to right window" })
